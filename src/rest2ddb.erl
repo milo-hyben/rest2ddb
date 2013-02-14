59da7748efb377bdfbefb595ae05e97b27628350
Milo Hyben <milo.hyben@datanoesis.com>
2013-02-14 22:51:40 +1100

    Preparing for put,post,patch,delete, fixing extract_query.

--- src/rest2ddb.erl
+++ src/rest2ddb.erl
@@ -8,47 +8,72 @@
 %% ====================================================================
 
 -export([
-		init/4,
-		get/2,
-        extract_query/1,
-        extract_path/1
-        %,
-		%put/3,
-		%post/2
-        %delete
-		]).
+	init/4,
+	get/2,
+	put/3,
+	post/3,
+	patch/3,
+  delete/2,
+  extract_query/1,
+  extract_path/1,
+  reload_schema/0
+	]).
 
 -spec init(string(), string(), pos_integer(), string()) -> 'ok'.
 %% @doc read tables schema information from DynamoDB and store it in application environment settings
 init(AccessKeyId, SecretAccessKey, DurationInSeconds, _Region) ->
-    ddb_iam:credentials(AccessKeyId, SecretAccessKey),
-    {'ok', Key, Secret, Token} = ddb_iam:token(DurationInSeconds),
-    ddb:credentials(Key, Secret, Token),
+  ddb_iam:credentials(AccessKeyId, SecretAccessKey),
+  {'ok', Key, Secret, Token} = ddb_iam:token(DurationInSeconds),
+  ddb:credentials(Key, Secret, Token),
 	{'ok', Tables} = ddb:tables(),
 	'ok' = application:set_env('rest2ddb', 'aws_tables', Tables),
 	'ok' = application:set_env('rest2ddb', 'aws_tables_details', parse_tables(Tables, [])).
-    
+
 -spec get([binary()], [binary()]) -> {ok, [[{binary(), [{binary(), binary()}]}]]} | {error, []}.
-%% @doc retrieve records from DynamoDB matching {UrlPath, UrlParameters}   
+%% @doc retrieve records from DynamoDB matching {UrlPath, UrlParameters}
 %% where UrlPath is list
 get(UrlPath, UrlParameters) ->
-	rest2ddb_utils:find_resource(UrlPath, UrlParameters).
-    
--spec extract_query(binary()) -> [binary()].    
+	rest2ddb_utils:get_resource(UrlPath, UrlParameters).
+
+%% @doc update existing or create new record
+put(UrlPath, UrlParameters, Attributes) ->
+	rest2ddb_utils:put_resource(UrlPath, UrlParameters, Attributes).
+
+%% @doc create new record
+post(UrlPath, UrlParameters, Attributes) ->
+	rest2ddb_utils:post_resource(UrlPath, UrlParameters, Attributes).
+
+%% @doc partial update of existing record (only selected attributes)
+patch(UrlPath, UrlParameters, Attributes) ->
+	rest2ddb_utils:patch_resource(UrlPath, UrlParameters, Attributes).
+
+%% @doc delete existing record
+delete(UrlPath, UrlParameters) ->
+	rest2ddb_utils:delete_resource(UrlPath, UrlParameters).
+
+-spec extract_query(binary()) -> [binary()].
 %% @doc Convert Query String part of URL into a list
-extract_query(Path) ->    
-    case binary:split(Path, [<<"?">>]) of
-        [_, Qs] -> token_binary(Qs, [<<"&">>], []);
-        [_]     -> <<>>
-    end.
+extract_query(Path) ->
+  case binary:split(Path, [<<"?">>]) of
+    [_, Qs] -> split_key_value( token_binary(Qs, [<<"&">>], []), []);
+    [_]     -> <<>>
+  end.
 
 -spec extract_path(binary()) -> [binary()].
 %% @doc Convert Path part of URL into a list
-extract_path(Path) ->    
-    case binary:split(Path, [<<"?">>]) of
-        [Raw, _] -> token_binary(Raw, [<<"/">>], []);
-        [_]     -> <<>>
-    end.
+extract_path(Path) ->
+  case binary:split(Path, [<<"?">>]) of
+    [Raw, _] -> token_binary(Raw, [<<"/">>], []);
+    [_]     -> <<>>
+  end.
+
+-spec reload_schema() -> 'ok'.
+%% @doc reload tables schema information from DynamoDB and store it in application environment settings
+reload_schema() ->
+	{'ok', Tables} = ddb:tables(),
+	'ok' = application:set_env('rest2ddb', 'aws_tables', Tables),
+	'ok' = application:set_env('rest2ddb', 'aws_tables_details', parse_tables(Tables, [])).
+
 
 %% ====================================================================
 %% Internal functions
@@ -72,10 +97,18 @@ extract_path(Path) ->
 
 %% @doc token_binary
 token_binary(B, Token, Acc) ->
-    case binary:split(B, Token) of
-        [A, T] -> token_binary(T, Token, Acc++[A]);
-        [Rest] -> Acc++[Rest]
-    end.
+  case binary:split(B, Token) of
+    [A, T] -> token_binary(T, Token, Acc++[A]);
+    [Rest] -> Acc++[Rest]
+  end.
+
+split_key_value([], Acc) -> Acc;
+split_key_value([H | T], Acc) ->
+  case binary:split(H, [<<"=">>]) of
+    [K, V] -> split_key_value(T, Acc++[{K, V}]);
+    [Rest] -> Acc++[{Rest, false}]
+  end.
+
 
 %% @doc parse aws base attribute
 parse_aws_attribute([{<<"AttributeName">>,Name},{<<"AttributeType">>,<<"S">>}]) ->
@@ -91,8 +124,8 @@ parse_table({ok,[{<<"Table">>,[ _,_,{<<"KeySchema">>
 							,[{<<"HashKeyElement">>,HashKey}]}
 							,_,{<<"TableName">>,TableName},_,_]}]}) ->
 	#table{name = TableName,
-		   hash_key_element = parse_aws_attribute(HashKey)
-		  };
+		hash_key_element = parse_aws_attribute(HashKey)
+	};
 
 %% @doc parse table with HashKey & RangeKey
 parse_table({ok,[{<<"Table">>,[ _,_,{<<"KeySchema">>
@@ -100,9 +133,9 @@ parse_table({ok,[{<<"Table">>,[ _,_,{<<"KeySchema">>
 							,{<<"RangeKeyElement">>,RangeKey}]}
 							,_,{<<"TableName">>,TableName},_,_]}]}) ->
 	#table{name = TableName,
-		   hash_key_element = parse_aws_attribute(HashKey),
-		   range_key_element=parse_aws_attribute(RangeKey)
-		  }.
+	  hash_key_element = parse_aws_attribute(HashKey),
+	  range_key_element=parse_aws_attribute(RangeKey)
+	}.
 
 
 %% @doc parse tables
